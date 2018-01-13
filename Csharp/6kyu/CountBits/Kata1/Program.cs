794bca8801a32956aed3e49b8c3f6068ac1c9f6e
Dmitry Pisarenko <pisarenkod@gmail.com>
2018-01-14 00:09:57 +0300

    CountBits 6 kyu Kata add

---
+++ Csharp/6kyu/CountBits/Kata1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CountBits(10000));
+            Console.ReadKey();
+
+        }
+        public static int CountBits(int n)
+        {
+            int a = 0;
+            while (n / 2 >= 1)
+            {
+                if ((n % 2) == 1)
+                {
+                    a++;
+                }
+                n = n / 2;
+                if(n == 1)
+                {
+                    a++;
+                }
+            }
+            
+            return a;
+        }
+    }
+}
