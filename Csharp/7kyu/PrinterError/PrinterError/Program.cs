ec450c1aa390064bbcd1118d9c82ef4e85702b93
Dmitry Pisarenko <pisarenkod@gmail.com>
2018-01-14 00:06:46 +0300

    Printer error Kata add to 7kyu

---
+++ Csharp/7kyu/PrinterError/PrinterError/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace PrinterError
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, please enter your string:");
+            Console.WriteLine("Result is: {0}", PrinterError(Console.ReadLine()));
+            Console.ReadKey();
+        }
+        public static string PrinterError(String s)
+        {
+            string temp = "";
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            //Var 1:
+            //temp = s.Where(c => c > 'm').Count() + "/" + s.Length;
+
+            //Var 2:
+            int i = 0;
+            bool flag = false;
+            char[] colorSign = new char[] {'a','b','c','d','e','f','g','h','i','j','k','l','m'};
+            foreach(char c in s.ToCharArray())
+            {
+                foreach(char check in colorSign)
+                {
+                    if (c == check)
+                    {
+                        flag = false;
+                        break;
+                    }
+                    else
+                    {
+                        flag = true;
+                    }
+                }
+                if (flag) i++;
+                flag = false;
+            }
+            temp = i.ToString() + "/" + s.Length;
+            System.Threading.Thread.Sleep(10000);
+            stopwatch.Stop();
+            TimeSpan ts = stopwatch.Elapsed;
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",ts.Hours, ts.Minutes, ts.Seconds,ts.Milliseconds / 10);
+            Console.WriteLine("RunTime " + elapsedTime);
+            return temp;
+        }
+    }
+}
---
+++ Csharp/7kyu/PrinterError/PrinterError/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PrinterError")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("PrinterError")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("65d42501-8462-4285-9699-497f3f3005ac")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
